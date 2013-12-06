1130a981862e7ba4ae4c0fd1646418fc47a85a14
NetRMS <tomatoalive@gmail.com>
2013-12-06 15:29:11 +0900

    Update Stage2.js

--- Stage2.js
+++ Stage2.js
@@ -24,39 +24,6 @@
 		this.y = Math.round(y || 0);
 	}
 	
-	function BlockArray() {
-		this.blocks = [];
-		this.xIndex = [];
-		this.yIndex = [];
-	}
-	PosArray.prototype = {
-		push: function (block) {
-			this.blocks.push(block);
-			
-		},
-		remove: function (block) {
-			this.blocks.splice(this.blocks.indexOf(block), 1);
-			this.xIndex.splice(this.xIndex.indexOf(block), 1);
-			this.yIndex.splice(this.yIndex.indexOf(block), 1);
-		},
-		testX: function (x, low, high) {
-			if(low > high) {
-				return low;
-			}
-			
-			var middle = Math.round((low + high) / 2),
-			block = this[middle];
-			if(block.rect.x > x) {
-				return testX.call(this, x, low, middle - 1);
-			}
-			else if(block.rect.x < x) {
-				return testX.call(this, x, middle + 1, high);
-			}
-			else {
-				return middle;
-			}
-		}
-	};
 	/*
 	 * Index
 	 */
@@ -136,6 +103,76 @@
 			context.strokeRect(this.rect.x, this.rect.y , this.rect.w, this.rect.h);
 		}
 	};
+	
+	/*
+	 * BlockArray
+	 */
+	
+	function BlockArray() {
+		this.blocks = [];
+		this.xIndex = [];
+		this.yIndex = [];
+	}
+	
+	BlockArray.prototype = {
+		push: function (block) {
+			this.blocks.push(block);
+			
+			this.xIndex.splice(this.xSearch(block.rect.x), 0, block);
+			this.yIndex.splice(this.ySearch(block.rect.y), 0, block);
+		},
+		xSearch: function (x) {
+			return _.search(this.xIndex, x, function(_block) {
+				return _block.rect.x + _block.rect.w/2;
+			});
+		},
+		ySearch: function (y) {
+			return _.search(this.yIndex, y, function(_block) {
+				return _block.rect.y + _block.rect.h/2;
+			});
+		},
+		remove: function (block) {
+			this.blocks.splice(this.blocks.indexOf(block), 1);
+			this.xIndex.splice(this.xIndex.indexOf(block), 1);
+			this.yIndex.splice(this.yIndex.indexOf(block), 1);
+		},
+		getBoundRect: function () {
+			return new Rect(this.xIndex[0].rect.x, this.yIndex[0].rect.y,
+				this.xIndex[this.xIndex.length - 1].rect.x + this.xIndex[this.xIndex.length - 1].rect.w,
+				this.yIndex[this.yIndex.length - 1].rect.y + this.yIndex[this.yIndex.length - 1].rect.h);
+		},
+		getBlocksInRect: function (rect) {
+			var array = new BlockArray(),
+			low, high;
+			
+			low = this.xSearch(rect.x);
+			high = this.xSearch(rect.w + rect.x);
+			
+			for(var i=low, blocks=this.xIndex; i<high; i++) {
+				array.push(blocks[i]);
+			}
+			
+			low = this.ySearch.call(array, rect.y);
+			high = this.ySearch.call(array, rect.h + rect.y);
+			
+			return array.yIndex.slice(low, high);
+		},
+		get: function (index) {
+			return this.blocks[index];
+		},
+		find: function (block) {
+			return this.blocks.indexOf(block);
+		},
+		size: function () {
+			return this.blocks.length;
+		},
+		clear: function () {
+			this.blocks = [];
+			this.xIndex = [];
+			this.yIndex = [];
+		}
+	};
+	
 	/*
 	 * Stage
 	 */
@@ -146,8 +183,8 @@
 		this.blockLayer = document.createElement("canvas");
 		this.link = document.createElement("canvas");
 		this.index = new Index();
-		this.blocks = [];
-		this.selectedBlocks = [];
+		this.blocks = new BlockArray();
+		this.selectedBlocks = new BlockArray();
 		this.lastPos = new Pos();
 		this.curPos = new Pos();
 		this.selectable = null;
@@ -206,11 +243,12 @@
 		};
 	}
 	
-	Stage2.RESOLUTION = [1189, 841];
-	//Stage2.RESOLUTION = [841, 594];
+	//Stage2.RESOLUTION = [1189, 841];
+	Stage2.RESOLUTION = [841, 594];
 	//Stage2.RESOLUTION = [594, 420];
 	//Stage2.RESOLUTION = [420, 297];
 	//Stage2.RESOLUTION = [297, 210];
+	
 	Stage2.CANVAS_STYLE = "rgba(0, 0, 0, 0.5) 10px 10px 10px";
 	Stage2.COLOR_GUIDELINE = "rgba(192, 192, 192, 0.5"; // alpha silver
 	Stage2.COLOR_AREA = "rgba(105, 105, 105, 1"; // dim gray
@@ -223,10 +261,13 @@
 	Stage2.MODE_SHIFT = 1 << 4;
 	
 	Stage2.prototype = {
-		addBlock: function () {
+		addBlock: function (x, y) {
 			var block = new Block(this);
 			
 			block.index = this.index.add(block);
+			block.rect.x = x;
+			block.rect.y = y;
+			
 			this.blocks.push(block);
 			
 			return block;
@@ -236,8 +277,8 @@
 			block.shadow();
 		},
 		invalidate: function () {
-			for(var i=0, _i=this.blocks.length, blocks=this.blocks; i<_i; i++) {
-				this.drawBlock(blocks[i]);
+			for(var i=0, blocks=this.blocks, _i=blocks.size(); i<_i; i++) {
+				this.drawBlock(blocks.get(i));
 			}
 		},
 		getPos: function (event) {
@@ -265,13 +306,13 @@
 			 */
 			this.maskLayer.getContext("2d").clearRect(0, 0, this.maskLayer.width, this.maskLayer.height); // 초기화
 			
-			for(var i=0, _i=this.selectedBlocks.length, blocks=this.selectedBlocks; i<_i; i++) {
-				blocks[i].select();
+			for(var i=0, blocks=this.selectedBlocks, _i=blocks.size(); i<_i; i++) {
+				blocks.get(i).select();
 			}
 			
 			var block = this.index.get(this.shadowLayer.getContext("2d").getImageData(this.curPos.x, this.curPos.y, 1, 1).data);
 			if(block) {
-				if(this.selectedBlocks.indexOf(block) == -1) { // 선택되어 있는 경우는 hover 하지 말도록
+				if(this.selectedBlocks.find(block) == -1) { // 선택되어 있는 경우는 hover 하지 말도록
 					block.hover();
 				}
 				
@@ -293,7 +334,8 @@
 			this.isDragging = true;
 			
 			if(this.mode & Stage2.MODE_MOVE) {
-				this.selectedImage = this.maskLayer.getContext("2d").getImageData(this.dragStartPos)
+				var rect = this.BlockArray.getBoundRect();
+				this.selectedImage = this.maskLayer.getContext("2d").getImageData(rect.x, rect.y, rect.w, rect.h);
 			}
 		},
 		dragEnd: function () {
@@ -301,6 +343,12 @@
 			context = layer.getContext("2d");
 			
 			context.clearRect(0, 0, layer.width, layer.height); // 초기화
+			
+			if(this.mode & Stage2.MODE_AREA) {
+				for(var i=0, blocks=this.selectedBlocks, _i=blocks.size(); i<_i; i++) {
+					blocks.get(i).select();
+				}
+			}
 		},
 		drag: function () {
 			if(this.mode & Stage2.MODE_AREA) {
@@ -308,13 +356,21 @@
 				
 				this.drawSelectArea(pos1, pos2);
 			
-				for(var i=0, blocks=this.blocks, _i=blocks.length; i<_i; i++) {
-					var rect = blocks[i].rect;
+				var blocks = this.blocks.getBlocksInRect(new Rect(Math.min(pos1.x, pos2.x), Math.min(pos1.y, pos2.y), Math.abs(pos2.x - pos1.x), Math.abs(pos2.y - pos1.y)));
+				
+				for(var i=0, _i=blocks.length; i<_i; i++) {
+					blocks[i].select();
+				}
+				
+				// rect = this.selectedBlocks.getBoundRect()
+				/*
+				for(var i=0, blocks=this.blocks, _i=blocks.size(); i<_i; i++) {
+					var rect = blocks.get(i).rect;
 					
 					if(_.isPointInRect(new Pos(rect.x + rect.w/2, rect.y + rect.h/2), pos1, pos2)) {
-						blocks[i].select();
+						blocks.get(i).select();
 					}
-				}
+				}*/
 			}
 			else if(this.mode & Stage2.MODE_MOVE) {
 				var pos = new Pos(this.dragStartPos.x - this.curPos.x, this.dragStartPos.y - this.curPos.y);
@@ -343,6 +399,16 @@
 			context.strokeStyle = Stage2.COLOR_AREA;
 			context.lineWidth = Stage2.SIZE_AREA;
 			context.strokeRect(pos1.x, pos1.y, pos2.x - pos1.x, pos2.y - pos1.y);
+		},
+		test1: function () {
+			var layer = this.maskLayer,
+			context = layer.getContext("2d");
+			
+			context.font = "16px bold";
+			for(var i=0; i<this.blocks.xIndex.length; i++) {
+				context.fillStyle = "red";
+				context.fillText(i, this.blocks.xIndex[i].rect.x+5, this.blocks.xIndex[i].rect.y);
+			}
 		}
 	};
 	
@@ -384,7 +450,7 @@
 			}
 			
 			if(!(this.mode & Stage2.MODE_CTRL || this.mode & Stage2.MODE_SHIFT)) {
-				this.selectedBlocks = [];
+				this.selectedBlocks.clear();
 			}
 			
 			var block = this.selectable;
@@ -396,7 +462,7 @@
 			else {
 				this.mode |= Stage2.MODE_MOVE;
 				
-				var index = this.selectedBlocks.indexOf(block);
+				var index = this.selectedBlocks.find(block);
 				
 				if(index == -1) {
 					this.selectedBlocks.push(block);
@@ -409,8 +475,8 @@
 			
 			//this.offset = new Pos(this.curPos.x/this.ratio - block.rect.x, this.curPos.y/this.ratio - block.rect.y);
 			
-			for(var i=0, _i=this.selectedBlocks.length, blocks=this.selectedBlocks; i<_i; i++) { // 마스크 새로 그리기
-				blocks[i].select();
+			for(var i=0, blocks=this.selectedBlocks, _i=blocks.size(); i<_i; i++) { // 마스크 새로 그리기
+				blocks.get(i).select();
 			}
 		},
 		onMouseUp: function (event) {
@@ -424,6 +490,7 @@
 			else {
 				//
 			}
+			
 			/*
 			var index = this.selectedBlocks.indexOf(block);
 			if(index > -1) {
