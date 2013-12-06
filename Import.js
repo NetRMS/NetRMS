e14159c69b2921829a6422a9a204a17bdde560fd
NetRMS <tomatoalive@gmail.com>
2013-12-06 15:28:26 +0900

    Update Import.js

--- Import.js
+++ Import.js
@@ -23,16 +23,63 @@
 		},
 		"util" : {
 			numToRGBString : function (n) {
-				return "#"+ this.numToHexCh((n >> 16) & 255) + this.numToHexCh((n >> 8) & 255) + this.numToHexCh(n & 255);
-			},
-			numToHexCh : function (n) {
-				var s = n.toString(16);
+				if(n > 0xffffff) throw ("invalid arguments "+ n);
 				
-				return s.length == 1? "0"+s: s;
+				return "#"+ (1 << 24 | n).toString(16).substring(1);
 			},
 			arrayToRGBNumber : function (array) {
 				return array[0]<<16 | array[1]<<8 | array[2];
-			}
+			},
+			arrayToRGBString: function (array) {
+				return "#"+ (1 << 24 | array[0]<<16 | array[1]<<8 | array[2]).toString(16).substring(1);
+			},
+			isPointInRect: function (p, p1, p2) {
+				var lowX , lowY, highX, highY;
+				
+				if(p1.x < p2.x) {
+					lowX = p1.x;
+					highX = p2.x;
+				}
+				else {
+					lowX = p2.x;
+					highX = p1.x;
+				}
+				
+				if(p1.y < p2.y) {
+					lowY = p1.y;
+					highY = p2.y;
+				}
+				else {
+					lowY = p2.y;
+					highY = p1.y;
+				}
+				
+				return lowX < p.x && lowY < p.y && highX > p.x && highY > p.y;
+			},
+			search: function (array, value, func) {
+				function _search(low, high) {
+					if(low > high) {
+						return low;
+					}
+					
+					var middle = Math.round((low + high) / 2),
+					element = array[middle],
+					result = func(element);
+					
+					if(result > value) {
+						return _search(low, middle - 1);
+					}
+					else if(result < value) {
+						return _search(middle + 1, high);
+					}
+					else {
+						return middle;
+					}
+				}
+				
+				return _search(0, array.length - 1);
+			},
+			
 		}
 	};
 
