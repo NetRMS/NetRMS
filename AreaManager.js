c7c1413dcffafbf0d4c1c3fc5a5ea5e5f48cdf05
NetRMS <tomatoalive@gmail.com>
2013-12-20 17:58:04 +0900

    Update AreaManager.js

--- AreaManager.js
+++ AreaManager.js
@@ -2,7 +2,7 @@
 
 	"use strict";
 
-	//var _ = extend("util");
+	var _ = extend("coord");
 	
 	/*
 	 * Area Manager
@@ -11,44 +11,61 @@
 	function AreaManager (stage) {
 		this.initialize(stage);
 	}
+	
+	/*
+	 * Extand
+	 */
+	
 	AreaManager.prototype = new StageManager();
+	
+	/*
+	 * Override
+	 */
+	
 	AreaManager.prototype.initialize = function (stage) {
-		StageManager.prototype.initialize.call(this,stage);
+		StageManager.prototype.initialize.call(this);
 		
-		stage.on("dragstart", this.reDraw.bind(this));
-		stage.on("drag", this.reDraw.bind(this));
-		stage.on("dragend", this.reDraw.bind(this));
+		this.draggable = null;
+		this.pos = {};
+		
+		stage.on("areaselectstart", this.onAreaSelectStart.bind(this));
+		stage.on("areaselect", this.onAreaSelect.bind(this));
+		stage.on("areaselectend", this.onAreaSelectEnd.bind(this));
+		stage.on("reset", this.onReset.bind(this));
 	};
 	AreaManager.prototype.set = function () {
 		this.context.strokeStyle = "rgb(105, 105, 105)";
 		this.context.lineWidth = 1;
-		this.context.fillStyle = "rgba(105, 105, 105, 0.2)";
+		this.context.fillStyle = "rgba(105, 105, 105, 0.4)";
+		//this.context.fillStyle = "rgba(0, 255, 0, 0.5)";
 	};
-	AreaManager.prototype.reDraw = function (event) {
-		if (event.block || (stage.mode & Stage.MODE_MOVE)) {
-			return;
-		}
+	
+	/*
+	 * Event Handler
+	 */
+	
+	AreaManager.prototype.onAreaSelectStart = function (event) {
+		this.pos = event.pos;
+	};
+	AreaManager.prototype.onAreaSelect = function (event) {
+		var rect = this.getZoomRect(_.getRect(this.pos, event.pos));
 		
 		this.clear();
 		
-		if(event.type == "drag") {
-			var rect = event.rect;
-	
-			this.context.beginPath();
-			this.context.rect(rect.x, rect.y, rect.w, rect.h);
-			this.context.closePath();
-			
-			this.context.stroke();
-			this.context.fill();
-		}
-		else if (event.type == "dragstart") {
-			stage.cursor("crosshair");
-		}
-		else if (event.type == "dragend") {
-			stage.cursor("default");
-		}
+		this.context.beginPath();
+		this.context.rect(rect.x, rect.y, rect.w, rect.h);
+		this.context.closePath();
+		
+		this.context.stroke();
+		this.context.fill();
+	};
+	AreaManager.prototype.onAreaSelectEnd = function (event) {
+		this.clear();
+	};
+	AreaManager.prototype.onReset = function (event) {
+		this.reset(event.width, event.height, event.zoom);
 	};
 	
 	window.AreaManager = AreaManager;
-	 
+	
 }) (window);
