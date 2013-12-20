cb3b3471b2efb558c946ffc4617ad1ee72c40848
NetRMS <tomatoalive@gmail.com>
2013-12-20 18:02:25 +0900

    Update TraceManager.js

--- TraceManager.js
+++ TraceManager.js
@@ -1,7 +1,7 @@
 (function (window, undefined) {
 
 	"use strict";
-
+	
 	//var _ = extend("util");
 	
 	function TraceManager (stage) {
@@ -19,23 +19,14 @@
 	 */
 	
 	TraceManager.prototype.initialize = function (stage) {
-		StageManager.prototype.initialize.call(this, stage);
+		StageManager.prototype.initialize.call(this);
 		
-		stage.on("add", this.onAddBlock.bind(this));
+		stage.on("block", this.onAddBlock.bind(this));
+		stage.on("link", this.onAddLink.bind(this));
 		stage.on("invalidate", this.invalidate.bind(this));
-		stage.on("dragend", this.onDragEnd.bind(this));
-	};
-	TraceManager.prototype.set = function () {
-	};
-	TraceManager.prototype.drawBlock = function (block) {
-		var context = this.context, rect = block.rect;
-		
-		context.beginPath();
-		context.rect(rect.x - rect.w/2, rect.y -rect.h/2, rect.w, rect.h);
-		context.closePath();
-		
-		context.fillStyle = block.index;
-		context.fill();
+		stage.on("stagemoveend", this.onDragEnd.bind(this));
+		stage.on("blockmoveend", this.onDragEnd.bind(this));
+		stage.on("reset", this.onReset.bind(this));
 	};
 	
 	/*
@@ -43,21 +34,46 @@
 	 */
 	
 	TraceManager.prototype.onAddBlock = function (event) {
-		this.drawBlock(event.block);
+		this.drawItem(event.block);
+	};
+	TraceManager.prototype.onAddLink = function (event) {
+		this.drawItem(event.link);
+	};
+	TraceManager.prototype.onDragStart = function (event) {
+		if (event.block && event.cursor == "crosshair") {
+			
+		}else {
+			this.copy();
+			this.clear();
+		}
 	};
 	TraceManager.prototype.onDragEnd = function (event) {
-		if (event.mode != Stage.MODE_MOVE) {
-			return;
-		}
-		
-		this.clear();
-		
 		this.invalidate();
+	};
+	TraceManager.prototype.onReset = function (event) {
+		this.reset(event.width, event.height, event.zoom);
+	};
+	
+	/*
+	 * Override
+	 */
+	
+	TraceManager.prototype.set = function () {
+	};
+	TraceManager.prototype.drawItem = function (item) {
+		item.path(this.context);
+		
+		this.context.fillStyle = item.index;
+		this.context.fill();
 	};
 	
 	/*
 	 * Method
 	 */
+	
+	TraceManager.prototype.getTrace = function (pos) {
+		return this.context.getImageData(pos.x, pos.y, 1, 1).data;
+	};
 	
 	window.TraceManager = TraceManager;
 
